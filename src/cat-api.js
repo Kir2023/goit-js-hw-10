1936af8d9aed7005d628fa31afdf1bdabf69b6c2
Kir2023 <132088682+Kir2023@users.noreply.github.com>
2023-09-07 16:33:09 +0300

    Done

---
+++ src/cat-api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const url = 'https://api.thecatapi.com/v1';
+
+axios.defaults.headers.common["x-api-key"] = "live_PPzkRx4kUWgPaxPu6pslxXlnWO2YM9K8Rc1XNSSSEb2463djXMaHwUy3SBw1CkUZ";
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+
+export function fetchBreeds() {
+    return axios.get(`${url}/breeds`)
+        .then(response => {
+            return response.data;
+        }).catch (error => {
+            throw new Error(error.message);
+        });       
+};
+
+export function fetchCatByBreed(breedId) {
+    return axios.get(`${url}/images/search?breed_ids=${breedId}`)
+        .then(response => {
+            return response.data;
+        });  
+};
